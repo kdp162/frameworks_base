3eb2ce5e2ff354983bce59a6594b931d6eec2368
ezio84 <brabus84@gmail.com>
2017-12-16 19:19:33 +0100

    Ticker: fix empty views on configuration changes
    to replicate: toggle navbar off and on again, tick something
    
    Change-Id: I1d9b075877da1ff917f2f51701dae939cb0be8f6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1759,6 +1759,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         mTickerEnabled = 0;
     }
 
+    private void refreshTicker() {
+        haltTicker();
+        if (mStatusBarView != null && mTickerEnabled != 0 && mTicker != null) {
+                mTicker.setStatusBarView(mStatusBarView);
+        }
+    }
+
     public int getStatusBarHeight() {
         if (mNaturalBarHeight < 0) {
             final Resources res = mContext.getResources();
@@ -4236,6 +4243,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mSlimRecents != null) {
             mSlimRecents.onConfigurationChanged(newConfig);
         }
+
+        refreshTicker();
     }
 
     public void userSwitched(int newUserId) {

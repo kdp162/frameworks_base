4cbf74ae87d238733e1f8e5d620d5f832a7f611f
Dan Cartier <nospamdan1@gmail.com>
2018-01-23 13:34:59 -0500

    Sort this mess out already...
    Change-Id: I485b9a7e3ca6fc7645d15476bbe4af640573c80b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5873,7 +5873,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private CandySettingsObserver mCandySettingsObserver = new CandySettingsObserver(mHandler);
     private class CandySettingsObserver extends ContentObserver {
-       ValidusSettingsObserver(Handler handler) {
+       CandySettingsObserver(Handler handler) {
             super(handler);
         }
 

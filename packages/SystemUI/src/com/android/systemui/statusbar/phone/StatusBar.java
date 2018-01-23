03a43daece8fbb521af3056e597e928d7938dc3a
Dan Cartier <nospamdan1@gmail.com>
2018-01-23 13:31:15 -0500

    fwb: Fix QS quick pulldown
    * An admitted mess... but this fixes:
      - QS quick pull down
      - Status bar double tap to sleep
      - And hopefully QS footer warnings
    
    Change-Id: I7e70b66834ac103c305e443577a1c4bc942aa4ea
    Signed-off-by: Dan Cartier <nospamdan1@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -895,6 +895,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         createAndAddWindows();
 
         mSettingsObserver.onChange(false); // set up
+        mCandySettingsObserver.observe();
+        mCandySettingsObserver.update();
         mCommandQueue.disable(switches[0], switches[6], false /* animate */);
         setSystemUiVisibility(switches[1], switches[7], switches[8], 0xffffffff,
                 fullscreenStackBounds, dockedStackBounds);
@@ -5879,9 +5881,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         public void onChange(boolean selfChange, Uri uri) {
             if (uri.equals(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
-                setLockscreenDoubleTapToSleep();
-            } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
                 setStatusBarWindowViewOptions();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN))) {
@@ -5890,14 +5889,11 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         public void update() {
-            setLockscreenDoubleTapToSleep();
-        }
-    }
-
-    private void setLockscreenDoubleTapToSleep() {
-        if (mStatusBarWindow != null) {
-            mStatusBarWindow.setLockscreenDoubleTapToSleep();
             setStatusBarWindowViewOptions();
+            setLockscreenMediaMetadata();
+            setStatusbarBatterySaverColor();
+            setQsPanelOptions();
+            updateQsPanelResources();
         }
     }
 
